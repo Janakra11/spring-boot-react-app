a31b9aa0cc1954926a3a6adf2e9877a09293d920
janakraj <janakrajkhatal@gmail.com>
2024-11-16 08:24:21 +0530

    updated exception class

--- fullstack-backend/src/main/java/com/codewithjanak/fullstack_backend/exception/UserNotFoundException.java
+++ fullstack-backend/src/main/java/com/codewithjanak/fullstack_backend/exception/UserNotFoundException.java
@@ -4,8 +4,4 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(Long id){
         super("Could not find id="+id);
     }
-
-    public UserNotFoundException(String message, Long id){
-        super(message+ " Id:"+id);
-    }
 }
